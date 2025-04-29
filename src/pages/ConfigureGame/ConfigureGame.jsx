d13066e667819067d15afb1e403f879902645836
AzureAD\LukasMartinezGnüchte <lukamartinez869@gmail.com>
2025-04-29 14:35:57 +0200

    teamauswahl gestyled

--- src/pages/ConfigureGame/ConfigureGame.jsx
+++ src/pages/ConfigureGame/ConfigureGame.jsx
@@ -3,7 +3,7 @@ import "./ConfigureGame.css";
 import Header from "./ConGamcomponents/Header.jsx";
 
 export default function ConfigureGame() {
-    
+
     const [step, setStep] = useState("configure");
 
     const handleAdvance = () => {
@@ -12,72 +12,70 @@ export default function ConfigureGame() {
 
     if (step === "setup-teams") {
         return (
-         <div className="header">
+            
+           <>
             <Header/>
-            <div className="configure-game-page">
+            <main className="color">
                 <div className="team-form-container">
+                    <div className="team-option green">
+                        <div className="title">Team Names</div>
+                    </div>
+                    <div className="team-option orange">
+                        <div className="title">Alone</div>
+                    </div>
+                    <div className="team-option teal">
+                        <div className="title">Double</div>
+                    </div>
+                    <div className="team-option blue">
+                        <div className="title">Draw your partner</div>
+                    </div>
                     
-                    <h2>Setup Teams</h2>
-                    <p>Hier kannst du jetzt deine Teams zusammenstellen!</p>
-                    <div className="form-item">
-                        <label className="team-selection" htmlFor="team-names">Team Names</label>
-                        <input className="team-input" id="team-names" type="text" />
-                    </div>
-                    <div className="form-item">
-                        <label className="team-input" htmlFor="alone">Alone</label>
-                        <input id="alone" type="text" />
-                    </div>
-                    <div className="form-item">
-                        <label className="team-input"  htmlFor="double">Double</label>
-                        <input id="double" type="text" />
-                    </div>
-                    <div className="form-item">
-                        <label  className="team-input" htmlFor="draw">Draw your Partner</label>
-                        <input id="draw" type="text" />
-                    </div>
+                        <button className="team-button">Create Tournament</button>
+                    
                 </div>
-            </div>
-            </div>
+                </main>
+                </>
         );
     }
-    
+
     return (
         <div className="header">
-            <Header/>
-        <div className="configure-game-page">
-            
-            <div className="form-container">
-                <div className="form-item">
-                    <label htmlFor="players" className="form-label">Enter number of players</label>
-                    <input id="players" className="form-input" type="number" required/>
-                </div>
+            <Header />
+            <div className="configure-game-page">
 
-                <div className="form-item">
-                    <label htmlFor="goals" className="form-label">Goals to win</label>
-                    <input id="goals" className="form-input" type="number" required />
-                </div>
+                <div className="form-container">
+                    <div className="form-item">
+                        <label htmlFor="players" className="form-label">Enter number of players</label>
+                        <input id="players" className="form-input" type="number" required />
+                    </div>
 
-                <div className="form-item">
-                    <label htmlFor="length" className="form-label">Match length in minutes</label>
-                    <input id="length" className="form-input" type="number" required />
-                </div>
+                    <div className="form-item">
+                        <label htmlFor="goals" className="form-label">Goals to win</label>
+                        <input id="goals" className="form-input" type="number" required />
+                    </div>
 
-                <div className="form-item">
-                    <label htmlFor="points" className="form-label">Points per win</label>
-                    <input id="points" className="form-input" type="number" required/>
-                </div>
+                    <div className="form-item">
+                        <label htmlFor="length" className="form-label">Match length in minutes</label>
+                        <input id="length" className="form-input" type="number" required />
+                    </div>
 
-                <div className="form-item">
-                    <label htmlFor="date" className="form-label">Date</label>
-                    <input id="date" className="form-input" type="date" required />
-                </div>
+                    <div className="form-item">
+                        <label htmlFor="points" className="form-label">Points per win</label>
+                        <input id="points" className="form-input" type="number" required />
+                    </div>
 
-                <div style={{ display: "flex", justifyContent: "space-between" }}>
-                    <button className="cancel-button">Cancel</button>
-                    <button className="form-button" onClick={handleAdvance}>Advance</button>
+                    <div className="form-item">
+                        <label htmlFor="date" className="form-label">Date</label>
+                        <input id="date" className="form-input" type="date" required />
+                    </div>
+
+                    <div>
+                        <button className="cancel-button">Cancel</button>
+                        <button className="form-button" onClick={handleAdvance}>Advance</button>
+                    </div>
                 </div>
             </div>
         </div>
-        </div>
+        
     );
 }
