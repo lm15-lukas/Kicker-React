ae2cb0d10f9376f39e0ac1eb2954c212d40ab74a
AzureAD\LukasMartinezGnüchte <lukamartinez869@gmail.com>
2025-05-02 13:59:52 +0200

    Error Nachricht wenn nicht jedes Feld ausgefültl ist

--- src/pages/ConfigureGame/ConfigureGame.jsx
+++ src/pages/ConfigureGame/ConfigureGame.jsx
@@ -5,6 +5,10 @@ import Header from "./ConGamcomponents/Header.jsx";
 
 export default function ConfigureGame() {
 
+    const[reset,setReset] =useState({});
+
+    const [errors,setErrors]=useState({});
+
 
     const [formData, setFormData] = useState({
         players: "",
@@ -33,14 +37,29 @@ export default function ConfigureGame() {
     const showModal = (text) => {
         setModalText(text);
     }
-    const handleTeamNames = () => showModal("Team Names gedrückt");
-    const handleAlone = () => showModal("Alone gedrückt");
-    const handleDouble = () => showModal("Double gedrückt");
-    const handleDrawPartner = () => showModal("Draw your partner gedrückt");
+    const handleTeamNames = () => showModal("Team Names pressed");
+    const handleAlone = () => showModal("Alone pressed");
+    const handleDouble = () => showModal("Double pressed");
+    const handleDrawPartner = () => showModal("Draw your partner pressed");
     const [step, setStep] = useState("configure");
 
     const handleAdvance = () => {
-        setStep("setup-teams");
+        const newErrors ={};
+
+        if(!formData.players) newErrors.players = "PLease select a value";
+        if(!formData.goals) newErrors.goals = "Please select a value for Goals";
+        if(!formData.length || formData.length ==="no Value") newErrors.length = "Select a Match length";
+        if(!formData.points) newErrors.points = "Please select points per Match";
+        if(!formData.date) newErrors.date = "Select a date For the Tournament";
+
+        if(Object.keys(newErrors).length >0){
+            setErrors(newErrors);
+        }else{
+            setErrors({});
+            setStep("setup-teams");
+        }
+
+
     };
     if (step === "setup-teams") {
         return (
@@ -108,6 +127,7 @@ export default function ConfigureGame() {
                             <option value="12">12</option>
 
                         </select>
+                        {errors.players && <h3 className="error-text">{errors.players}</h3>}
                     </div>
 
                     <div className="form-item">
@@ -116,6 +136,7 @@ export default function ConfigureGame() {
                             value={formData.goals}
                             onChange={handleFormDataChange}
                         >
+                            <option value="null">-- Please enter --</option>
                             <option value="7">7</option>
                             <option value="1">1</option>
                             <option value="2">2</option>
@@ -127,6 +148,7 @@ export default function ConfigureGame() {
                             <option value="9">9</option>
                             <option value="10">10</option>
                         </select>
+                        {errors.goals && <h3 className="error-text">{errors.goals}</h3>}
                     </div>
                     <div className="form-item">
 
@@ -136,7 +158,8 @@ export default function ConfigureGame() {
                             value={formData.length}
                             onChange={handleFormDataChange}
                         >
-                            <option value="no Value">No time tracking</option>
+                            <option value="null">--Please Select--</option>
+                            <option value="2">No time Tracking</option>
                             <option value="5">5</option>
                             <option value="6">6</option>
                             <option value="7">7</option>
@@ -144,12 +167,14 @@ export default function ConfigureGame() {
                             <option value="9">9</option>
                             <option value="10">10</option>
                         </select>
+                        {errors.length && <h3 className="error-text">{errors.length}</h3> }
                     </div>
 
                     <div className="form-item">
                         <label htmlFor="points" className="form-label">Points per win</label>
                         <select
                             id="points" className="form-input" type="number" required value={formData.points} onChange={handleFormDataChange}>
+                            <option value="null">--Please enter--</option>
                             <option value="1">1</option>
                             <option value="2">2</option>
                             <option value="3">3</option>
@@ -161,14 +186,16 @@ export default function ConfigureGame() {
                             <option value="9">9</option>
                             <option value="10">10</option>
                         </select>
+                        {errors.points && <h3 className="error-text">{errors.points}</h3>}
 
                     </div>
                     <div className="form-item">
                         <label htmlFor="date" className="form-label">Date</label>
                         <input id="date" className="form-input" type="date" required value={formData.date} onChange={handleFormDataChange} />
+                        {errors.date && <h3 className="error-text">{errors.date}</h3>}
                     </div>
                     <div>
-                        <button className="cancel-button">Cancel</button>
+                        <button className="cancel-button">Reset</button>
                         <button className="form-button" onClick={handleAdvance}>Advance</button>
                     </div>
                 </div>
