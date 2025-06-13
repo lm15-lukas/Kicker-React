f4d4daf086b75a240d848259b53d55ab9b9fa452
AzureAD\LukasMartinezGnüchte <lukamartinez869@gmail.com>
2025-06-13 10:40:45 +0200

    Turnier kann mit EndTournament button beendet werden

--- src/pages/Tournament/TournamentComponents/TurnamentHeader.jsx
+++ src/pages/Tournament/TournamentComponents/TurnamentHeader.jsx
@@ -1,8 +1,14 @@
+import { useNavigate } from "react-router-dom";
 import Trophy from "../assets/images/trophy-solid.svg";
 export default function TournamentHeader() {
+    const navigatToScoreBoard = useNavigate();
+    const handleEndTournament =() =>{
+        navigatToScoreBoard('/score-board');
+    }
     return (
         <header className="header">
             <img src={Trophy} alt="trophy" className="trophy"/>
+            <button className="tournament-End" onClick={handleEndTournament}>End Tournament</button>
             <h1>Tournament XY</h1> 
         </header>
     )
