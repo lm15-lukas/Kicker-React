052f27bab9e4414444b812933847dc96e47a505e
AzureAD\LukasMartinezGnüchte <lukamartinez869@gmail.com>
2025-06-17 09:12:19 +0200

    LoadTournament css homzugefügt

--- src/pages/Tournament/TournamentComponents/LoadTournament.jsx
+++ src/pages/Tournament/TournamentComponents/LoadTournament.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import './LoadTournament.css';
 
 export default function LoadTournaments() {
     const [savedTournaments, setSavedTournaments] = useState([]);
